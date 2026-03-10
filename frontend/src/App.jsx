8deb8ceb78e6369093c1056bbc2b2643627f2db9
Rutej Talati <rutejtalati16@gmail.com>
2026-03-10 15:42:46 +0530

    fix: clean up App.jsx imports and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,8 @@ import PredictionsPage      from "./pages/PredictionsPage";
 import PlayerInsightPage    from "./pages/PlayerInsightPage";
 import PlayerBrowsePage     from "./pages/PlayerBrowsePage";
 import MiniGamesPage        from "./pages/MiniGamesPage";
-import GroundZeroPage from "./pages/Groundzeropage"
+import GroundZeroPage       from "./pages/Groundzeropage";
+
 const SLUG_MAP = {
   "premier-league":"epl","la-liga":"laliga","serie-a":"seriea","ligue-1":"ligue1",
   "epl":"epl","laliga":"laliga","seriea":"seriea","ligue1":"ligue1",
@@ -23,26 +24,27 @@ export default function App() {
       <div className="app-shell">
         <Navbar />
         <div className="sn-page-wrap">
-        <Routes>
-          <Route path="/"                   element={<HomePage />} />
-          <Route path="/squad-builder"      element={<SquadBuilderPage />} />
-          <Route path="/best-team"          element={<BestTeamPage />} />
-          <Route path="/gameweek-insights"  element={<GameweekInsightsPage />} />
-          <Route path="/player"             element={<PlayerBrowsePage />} />
-          <Route path="/player/:id"         element={<PlayerInsightPage />} />
-          <Route path="/fpl-table"          element={<FplTablePage />} />
-          <Route path="/games"              element={<MiniGamesPage />} />
-<Route path="/learn" element={<GroundZeroPage/>}/>          <Route path="/predictions"
-            element={<Navigate to="/predictions/premier-league" replace />} />
-          <Route path="/predictions/:league"
-            element={<PredictionsPage slugMap={SLUG_MAP} />} />
-          <Route path="/league/epl"    element={<LeaguePage league="epl" />} />
-          <Route path="/league/laliga" element={<LeaguePage league="laliga" />} />
-          <Route path="/league/seriea" element={<LeaguePage league="seriea" />} />
-          <Route path="/league/ligue1" element={<LeaguePage league="ligue1" />} />
-          <Route path="/simulation/:league" element={<SeasonSimulator />} />
-          <Route path="/simulation/epl"     element={<SeasonSimulator />} />
-        </Routes>
+          <Routes>
+            <Route path="/"                   element={<HomePage />} />
+            <Route path="/squad-builder"      element={<SquadBuilderPage />} />
+            <Route path="/best-team"          element={<BestTeamPage />} />
+            <Route path="/gameweek-insights"  element={<GameweekInsightsPage />} />
+            <Route path="/player"             element={<PlayerBrowsePage />} />
+            <Route path="/player/:id"         element={<PlayerInsightPage />} />
+            <Route path="/fpl-table"          element={<FplTablePage />} />
+            <Route path="/games"              element={<MiniGamesPage />} />
+            <Route path="/learn"              element={<GroundZeroPage />} />
+            <Route path="/predictions"
+              element={<Navigate to="/predictions/premier-league" replace />} />
+            <Route path="/predictions/:league"
+              element={<PredictionsPage slugMap={SLUG_MAP} />} />
+            <Route path="/league/epl"         element={<LeaguePage league="epl" />} />
+            <Route path="/league/laliga"      element={<LeaguePage league="laliga" />} />
+            <Route path="/league/seriea"      element={<LeaguePage league="seriea" />} />
+            <Route path="/league/ligue1"      element={<LeaguePage league="ligue1" />} />
+            <Route path="/simulation/:league" element={<SeasonSimulator />} />
+            <Route path="/simulation/epl"     element={<SeasonSimulator />} />
+          </Routes>
         </div>
 
         <footer className="site-footer">
